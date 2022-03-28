157f67930ca5faca1cf4ce01723226d949a15ddc
hazimabdo <hazimabdo67@gmail.com>
2022-03-29 01:19:47 +0300

    test message

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>261865e6-6212-4b26-ac13-9446f57ea534</testSuiteGuid>
+</TestSuiteEntity>
