6a21c560015783fdf7589b2822636df1779bead2
hazimabdo <hazimabdo67@gmail.com>
2022-03-29 04:18:27 +0300

    test message

--- Test Suites/Test Suite 1.ts
+++ Test Suites/Test Suite 1.ts
@@ -11,4 +11,16 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>261865e6-6212-4b26-ac13-9446f57ea534</testSuiteGuid>
+   <testCaseLink>
+      <guid>fe0f9947-1315-48d3-93e9-6148b04a47ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/kim test2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c15b8d39-5a3f-47f1-9fbb-f5cb583e30a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tom test1</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
